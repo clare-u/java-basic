50ac215a0cc24ff603eb9987cbb8f6859dbb20f9
ucream-sh <ucream612@gmail.com>
2024-03-07 14:31:45 +0900

    init java-basic

---
+++ 01_Class/src/class1/ClassStart3.java
@@ -0,0 +1,23 @@
+package class1;
+
+public class ClassStart3 {
+	public static void main(String[] args) {
+		Student student1;
+		// 학생을 담아둘 수 있는 변수를 선언 (1번 학생)
+		student1 = new Student(); // 학생을 실제 메모리에 만듬
+		// 메모리에 학생을 만들어서 쓸 수 있게 되고, student1변수명으로 사용
+		student1.name = "학생1";
+		// 클래스에서는 .을 통해 멤버변수에 접근 (배열에서 []로 접근했듯이)
+		student1.age = 15;
+		student1.grade = 20;
+
+		Student student2 = new Student();
+		student2.name = "학생2";
+		student2.age = 16;
+		student2.grade = 80;
+
+		System.out.println("이름: " + student1.name + " 나이: " + student1.age + " 성적: " + student1.grade);
+		// 스튜던트1번의 이름, 나이, 성적에 접근해서 출력
+		System.out.println("이름: " + student2.name + " 나이: " + student2.age + " 성적: " + student2.grade);
+	}
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+## 📖 김영한의 실전 자바 - 기본편
+
+🍃Inflearn의 `김영한의 실전 자바 - 기본편`을 학습하며 예제 코드를 작성합니다.
+
+[강의 링크](https://www.inflearn.com/course/%EA%B9%80%EC%98%81%ED%95%9C%EC%9D%98-%EC%8B%A4%EC%A0%84-%EC%9E%90%EB%B0%94-%EA%B8%B0%EB%B3%B8%ED%8E%B8#)
+
+### 사용 툴
+<img src="https://img.shields.io/badge/Java-007396?style=flat-square&logo=openjdk&logoColor=white"/> <img src="https://img.shields.io/badge/Eclipse IDE-2C2255?style=flat-square&logo=eclipseide&logoColor=white">
+
+### 목차
+
+1. 클래스와 데이터
+2. 기본형과 참조형
+3. 객체 지향 프로그래밍
+4. 생성자
+5. 패키지
+6. 접근 제어자
+7. 자바 메모리 구조와 static
+8. final
+9. 상속
+10. 다형성1
+11. 다형성2
+12. 다형성과 설계
+13. 다음으로
