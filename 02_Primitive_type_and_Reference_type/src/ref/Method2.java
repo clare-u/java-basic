99fc436c904558c2cef6d89d39fa130a62c5d247
clare-u <clare.u.dev@gmail.com>
2024-03-26 12:33:43 +0900

    [02_Primitive_type_and_Reference_type] Method2 참조값 출력 코드 추가

--- 02_Primitive_type_and_Reference_type/src/ref/Method2.java
+++ 02_Primitive_type_and_Reference_type/src/ref/Method2.java
@@ -5,9 +5,14 @@ public class Method2 {
 		// Method1 코드 리팩토링
 
 		Student student1 = createStudent("학생1", 15, 90);
-		// student1의 참조값 대입
+		System.out.println("student1= " + student1); // student1의 참조값 대입
+//		student = ref.Student@7a81197d
+//		student1= ref.Student@7a81197d
+
 		Student student2 = createStudent("학생2", 16, 80);
-		// student2의 참조값 대입
+		System.out.println("student2= " + student2); // student2의 참조값 대입
+//		student = ref.Student@3f99bd52
+//		student2= ref.Student@3f99bd52
 
 		printStudent(student1);
 		printStudent(student2);
@@ -16,6 +21,7 @@ public class Method2 {
 
 	static Student createStudent(String name, int age, int grade) {
 		Student student = new Student();
+		System.out.println("student = " + student);
 		student.name = name;
 		student.age = age;
 		student.grade = grade;
