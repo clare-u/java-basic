d8b62ccac21789997363857a608d0d7eca6f4c20
clare-u <clare.u.dev@gmail.com>
2024-04-03 15:36:19 +0900

    [02_Primitive_type_and_Reference_type] 멤버 변수와 null

---
+++ 02_Primitive_type_and_Reference_type/src/ref/NullMain4.java
@@ -0,0 +1,14 @@
+package ref;
+
+public class NullMain4 {
+	public static void main(String[] args) {
+		BigData bigData = new BigData();
+		bigData.data = new Data();
+		System.out.println("bigData.count= " + bigData.count); // 0
+		System.out.println("bigData.data= " + bigData.data); // ref.Data@5b2133b1
+
+		System.out.println("bigData.data.value= " + bigData.data.value);
+		// bigData.data.value= 0
+	}
+
+}
