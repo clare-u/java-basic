46e9dd3c023d7d19f9d8fa0b49b2851542f532be
clare-u <clare.u.dev@gmail.com>
2024-04-10 17:08:08 +0900

    [05_Package] 패키지 import

--- 05_Package/src/pack/PackageMain2.java
+++ 05_Package/src/pack/PackageMain2.java
@@ -1,12 +1,12 @@
 package pack;
 
-import pack.a.User;
+import pack.a.*;
 
-public class PackageMain1 {
+public class PackageMain2 {
 	public static void main(String[] args) {
 		Data data = new Data();
 		User user = new User();
-		// 임포트를 사용하면 다른 패키지의 클래스를 가져와서 사용 가능하다.
+		// 패키지에 포함된 모든 클래스를 포함해서 사용하려면 * 을 사용
 
 //		패키지 pack Data 생성
 //		패키지 pack.a 회원 생성
