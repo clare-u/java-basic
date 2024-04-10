7021fb5154cb39865c520599de50cf0b1ee2f13b
clare-u <clare.u.dev@gmail.com>
2024-04-10 17:10:53 +0900

    [05_Package] 클래스 이름 중복

---
+++ 05_Package/src/pack/PackageMain3.java
@@ -0,0 +1,14 @@
+package pack;
+
+import pack.a.User;
+
+public class PackageMain3 {
+	public static void main(String[] args) {
+		User userA = new User();
+		pack.b.User userB = new pack.b.User();
+
+//		패키지 pack.a 회원 생성
+//		패키지 pack.b 회원 생성
+	}
+
+}
