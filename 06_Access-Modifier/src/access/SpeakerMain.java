bd89f57b3117225d6e2be28f79a7fb5e04323b46
clare-u <clare.u.dev@gmail.com>
2024-04-11 17:33:51 +0900

    [06_Access-Modifier] 접근 제어자 이해 1 :  필드의 외부 접근을 막을 수 있는 방법

---
+++ 06_Access-Modifier/src/access/SpeakerMain.java
@@ -0,0 +1,20 @@
+package access;
+
+public class SpeakerMain {
+	public static void main(String[] args) {
+		Speaker speaker = new Speaker(90); // 초기음량은 90
+		speaker.showVolume(); // 현재 음량: 90
+
+		speaker.volumeUp(); // 음량을 10 증가합니다.
+		speaker.showVolume(); // 현재 음량: 100
+
+		speaker.volumeUp(); // 음량을 증가할 수 없습니다. 최대 음량입니다.
+		speaker.showVolume(); // 현재 음량: 100
+		
+		// 필드에 직접 접근
+		System.out.println("volume 필드 직접 접근 수정");
+		speaker.volume = 200;
+		speaker.showVolume(); // 현재 음량: 200
+	}
+
+}
