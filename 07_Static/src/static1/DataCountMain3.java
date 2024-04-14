1b189179446c3f76017ac87fe4359de75148e465
clare-u <clare.u.dev@gmail.com>
2024-04-14 18:31:16 +0900

    [07_Static] 정적 변수 접근법

--- 07_Static/src/static1/DataCountMain3.java
+++ 07_Static/src/static1/DataCountMain3.java
@@ -4,18 +4,26 @@ public class DataCountMain3 {
 	public static void main(String[] args) {
 		Data3 data1 = new Data3("A");
 		System.out.println("A count= " + Data3.count);
-		
+
 		Data3 data2 = new Data3("B");
 		System.out.println("B count= " + Data3.count);
-	
+
 		Data3 data3 = new Data3("C");
 		System.out.println("C count= " + Data3.count);
-	
+
 //		A count= 1
 //		B count= 2
 //		C count= 3
-		
+
 		// 공용 변수인 static으로 접근
+
+		// 추가
+		// 인스턴스를 통한 접근
+		Data3 data4 = new Data3("D");
+		System.out.println(data4.count); // 4
+
+		// 클래스를 통한 접근
+		System.out.println(Data3.count); // 4
 
 	}
 
