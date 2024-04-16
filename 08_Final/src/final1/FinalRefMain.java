3bf1517afffbb6f80780a6ebe62218c609781b73
clare-u <clare.u.dev@gmail.com>
2024-04-16 14:27:59 +0900

    [08_Final] final 변수와 참조

---
+++ 08_Final/src/final1/FinalRefMain.java
@@ -0,0 +1,16 @@
+package final1;
+
+public class FinalRefMain {
+	public static void main(String[] args) {
+		final Data data = new Data(); // 참조형
+		// data = new Data(); // 컴파일 에러
+		// 참조값은 변경 불가
+
+		// 참조 대상의 값은 변경 가능
+		data.value = 10;
+		System.out.println(data.value); // 10
+		data.value = 20;
+		System.out.println(data.value); // 20
+	}
+
+}
